6332ed121302f6c7300f2ceb074cfe02b9969618
neto683 <100849234+neto683@users.noreply.github.com>
2022-03-20 11:41:51 -0300

    condições

---
+++ aula 11/ex-008.js
@@ -0,0 +1,7 @@
+var vel = 90
+console.log(`A velocidade é de ${vel} KM/H`)
+if (vel > 60){ // condição simple por nao ter o se não
+    console.log(`Voce esta a ${vel} km/h voce sera multado a maxima permitida é 60km/h voce sera MULTADO!!!!!! EM 1.000 Reais`)
+}
+
+console.log(`Tenha um bom dia!`)
---
+++ aula 11/ex-009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+if (pais == 'Brasil'){
+  console.log('Brasileiro')
+}else{
+    console.log('Estrangeiro')
+}// cria se um variavel e podendendo ger duas condições dependeno do que vai esta na variavel, quando temos IF (q significa se isso acontecer) else( significa se não acontercer )
+//console.log('') é o comando para mostra no nodejs
