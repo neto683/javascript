777277626a0191dee19e2509e25e66ce60e2fa6e
neto683 <100849234+neto683@users.noreply.github.com>
2022-03-21 18:11:38 -0300

    ex-0011

---
+++ aula04/aula 09/aula 12/ex-0011.js
@@ -0,0 +1,9 @@
+var idade =18
+if(idade>18){
+  console.log('Obrigatorio o voto')
+} else if(idade ==16){
+  console.log('Voto não obrigatorio')
+}else if(idade<18){
+  console.log('Não pode votar')
+}
+
