dcf087abc099dd26b31e6103e4c0a33dd140b444
neto683 <100849234+neto683@users.noreply.github.com>
2022-03-22 20:22:57 -0300

    exercicio de verificação de idade

--- aula 12/aula 12ex/script/script-2.js
+++ aula 12/aula 12ex/script/script-2.js
@@ -1,5 +1,42 @@
 function clicar(){
-  var ano =window.document.getElementById('cNum')
-  var seco = window.document.getElementsByName('cSex')
-  var bt = window.document.getElementById('botao')
+  var data = new Date()
+  var ano =data.getFullYear()
+  var fano = document.getElementById('cNum')
+  var res = document.getElementById('res')
+  var foto =document.getElementById('mulher')
+  if(fano.value.length == 0|| Number(fano.value)> ano){
+    window.alert('ERRO')
+  }else{
+    var fsex = document.getElementsByName('cSex')
+    var idade = ano -Number(fano.value)
+    var gernero= ''
+    var img= document.createElement('img')
+    img.setAttribute('id', 'foto')
+    if(fsex[0].checked){
+      gernero= 'Homem'
+      if(idade>= 0 && idade< 10)
+      img.setAttribute('src','imagens/homem-bebe.png')
+      else if (idade< 21){
+        img.setAttribute('src','imagens/homem-jovem.png')
+      }else if(idade<50){
+        img.setAttribute('src', 'imagens/homem-adulto.png')
+      }else{
+        img.setAttribute('src','imagens/homem-idoso.png')
+      }
+    }else if(fsex[1].checked){
+      gernero='Mulher'
+      if(idade>= 0 && idade< 10)
+      img.src='imagens/mulher-bebe.png'
+      else if (idade< 21){
+        img.src='imagens/mulher-jovem.png'
+      }else if(idade<50){
+        img.src='imagens/mulher-adulta.png'
+      }else{
+        img.src='imagens/mulher-idosa.png'
+      }
+    }
+    res.style.textAlign='center'
+    res.innerHTML=`Detectamos ${gernero} com ${idade} anos`
+    res.appendChild(img)
+  }
 }
