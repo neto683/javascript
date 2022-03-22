10f5327e1cbaa204e710f3ec4739f1d97e444f02
neto683 <100849234+neto683@users.noreply.github.com>
2022-03-22 00:32:00 -0300

    exercicio de javaScript

--- aula 12/aula 12ex/script/script.js
+++ aula 12/aula 12ex/script/script.js
@@ -1,7 +1,21 @@
 function carregar(){
-var agora = new Date()
-var hora = agora.getHours()
-var p = window.document.getElementById('hora')
-p.innerHTML = 'Agora são ' + hora + ' horas.'
-p.style.textAlign ='center'
+  var msg = window.document.getElementById('msg')
+  var img =window.document.getElementById('imagem')
+  var data= new Date()
+  var hora = data.getHours()
+  msg.innerHTML = `Agora são ${hora} horas.`
+  msg.style.textAlign='center'
+   if (hora >= 0 && hora < 12) {
+     console.log('caino de 12')
+     document.body.style.backgroundColor='#e4cac0'
+    img.src = 'imagens/fotomanha.png'
+    }else if (hora > 12 && hora < 18){
+      console.log('caino de 13')
+    img.src= 'imagens/fototarde.png'
+    document.body.style.backgroundColor='#a96c71'
+    }else{
+      console.log('caino de 14')
+    img.src='imagens/fotonoite.png'
+    document.body.style.backgroundColor='#042024'
+    }
 }
