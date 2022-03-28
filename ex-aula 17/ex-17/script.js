873c281e32eb7b00142fb4a2893195274ef9f460
neto683 <100849234+neto683@users.noreply.github.com>
2022-03-28 12:25:52 -0300

    ex

---
+++ ex-aula 17/ex-17/script.js
@@ -0,0 +1,10 @@
+function adicionar(){
+  var numero = window.document.getElementById('cNum')
+  var res = window.document.getElementById('cSelec')
+  var item = document.createElement('option')
+  var n = Number(numero.value)
+ for(var c=1;c<n;c++){
+   res.innerHTML= item
+ }
+}
+function finalizar()
